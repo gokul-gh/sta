538afcdfe1dd1fb3cf75cfe33349cfab2a99ca52
gokul <nkl.gokulb@gmail.com>
2024-05-31 16:38:21 +0530

    Chapter 6 FixIt 1 - Initial commit

---
+++ js/CH6-fixIt1/JSB006_000_001/Product.js
@@ -0,0 +1,5 @@
+ function() productNumbers(firstNumber, secondNumber) {
+           firstNumber * secondNumber;
+        }
+        const product = productNumbers(4, 6);
+        console.log(product); 

13a1437059f736c6ff8b5882b06967b450a2e20d
gokul <nkl.gokulb@gmail.com>
2024-06-03 13:12:29 +0530

    Chapter 7 - fix coding format

---
+++ js/CH7-fixIt1/BUGFIX-11.js
@@ -0,0 +1,9 @@
+const age = 18;
+
+//condition to find if the age requirement has met
+if (age>=18) {
+    console.log("You are eligible to vote.");
+}
+else {
+    console.log("You are not eligible to vote.");
+}

029bd85fe5c2f38737ccaa6ddffdb9093f14bd03
gokul <nkl.gokulb@gmail.com>
2024-06-05 10:07:51 +0530

    Chapter-7 Engineering college admission system - Initial commit

---
+++ js/CH7-challenges/engineering-college-admission-system/CH_ID_0021/CH_ID_0021/Admission_System.js
@@ -0,0 +1,21 @@
+//Date and Time Declaration
+let displayDate = new Date();
+document.getElementById("datOutput").innerHTML = displayDate.toLocaleDateString();
+document.getElementById("timOutput").innerHTML = displayDate.toLocaleTimeString();
+const copyText = document.querySelector("#copy");
+copyText.addEventListener("click", () => {
+navigator.clipboard.writeText(document.querySelector("#successCode").value);
+copyText.textContent = "copied";
+setTimeout(() => {
+copyText.innerHTML = `<span>&#128203; </span>copy`;
+}, 2000);
+});
+
+//Modal
+const toggleSuccessModal = () => successModal.classList.toggle("active");
+const toggleErrorModal = () => errorModal.classList.toggle("active");
+window.addEventListener("click", function (event) {
+if (event.target === successModal) successModal.classList.remove("active");
+if (event.target === errorModal) errorModal.classList.remove("active");
+});
+//Code Logic

b3b9d4c6b42ce4cc8c936031daa4dc4af17f22f0
gokul <nkl.gokulb@gmail.com>
2024-06-07 00:04:31 +0530

    Chapter-8 Palindrome number - Initial commit

---
+++ js/CH8-challenges/palindrome-number/CH_ID_0028/CH_ID_0028/Palindrome Number.js
@@ -0,0 +1,20 @@
+//Date and Time Declration
+let displayDate = new Date();
+
+//Code Logic
+const copyText = document.querySelector("#copy");
+copyText.addEventListener("click", () => {
+navigator.clipboard.writeText(document.querySelector("#successCode").value);
+copyText.textContent = "copied";
+setTimeout(() => {
+copyText.innerHTML = `<span>&#128203; </span>copy`;
+}, 2000);
+});
+
+//Modal
+const toggleSuccessModal = () => successModal.classList.toggle("active");
+const toggleErrorModal = () => errorModal.classList.toggle("active");
+window.addEventListener("click", function (event) {
+if (event.target === successModal) successModal.classList.remove("active");
+if (event.target === errorModal) errorModal.classList.remove("active");
+});

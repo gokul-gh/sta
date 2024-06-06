e9974c2bacf654fc08317843a4b690202e77467e
gokul <nkl.gokulb@gmail.com>
2024-06-06 23:37:04 +0530

    Chapter-8 - print numbers using while loop

--- js/CH8-challenges/print-numbers-while-loop/CH_ID_0018/CH_ID_0018/Print Numbers.js
+++ js/CH8-challenges/print-numbers-while-loop/CH_ID_0018/CH_ID_0018/Print Numbers.js
@@ -1,26 +1,35 @@
+//Variables declaration
+let firstNumberId = document.getElementById("firstNumber");
+let secondNumberId = document.getElementById("secondNumber");
+let outputArea = document.getElementById("resultId");
+
+function whileLoop() {
+  // Code statements
+  outputArea.value = "";
+  let firstNumberValue = firstNumberId.value;
+  let secondNumberValue = secondNumberId.value;
+  let iteration = firstNumberValue;
+  while (iteration <= secondNumberValue) {
+    outputArea.value += `${iteration}\n`;
+    iteration++;
+  }
+}
+
 let displayDate = new Date();
 //copy to clipboard
 const copyText = document.querySelector("#copy");
 copyText.addEventListener("click", () => {
-navigator.clipboard.writeText(
-document.querySelector("#successCode").value
-);
-copyText.textContent = "copied";
-setTimeout(() => {
-copyText.innerHTML = `<span>&#128203; </span>copy`;
-}, 2000);
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
 });
 
 //Modal
 const toggleSuccessModal = () => successModal.classList.toggle("active");
 const toggleErrorModal = () => errorModal.classList.toggle("active");
 window.addEventListener("click", function (event) {
-if (event.target === successModal)
-successModal.classList.remove("active");
-if (event.target === errorModal) errorModal.classList.remove("active");
+  if (event.target === successModal) successModal.classList.remove("active");
+  if (event.target === errorModal) errorModal.classList.remove("active");
 });
-
-function whileLoop(){
-   // Code statements
-
-}

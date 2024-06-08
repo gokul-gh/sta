0fdfcd36ebb0dd94eddd1474f49668de80997d18
gokul <nkl.gokulb@gmail.com>
2024-06-08 21:09:15 +0530

    Chapter-2 output challenges

--- js/CH2-challenges/output-challenges-1/CH_ID_005/CH_ID_005/HelloWorld.js
+++ js/CH2-challenges/output-challenges-1/CH_ID_005/CH_ID_005/HelloWorld.js
@@ -1,5 +1,8 @@
 // Code statements
 
+document.getElementById("outputId").innerHTML = "Hello World";
+console.log("Hello World");
+
 let displayDate = new Date();
 document.getElementById("dateOutput").innerHTML =
   displayDate.toLocaleDateString();
@@ -9,9 +12,7 @@ document.getElementById("timeOutput").innerHTML =
 //copy to clipboard
 const copyText = document.querySelector("#copy");
 copyText.addEventListener("click", () => {
-  navigator.clipboard.writeText(
-    document.querySelector("#successCode").value
-  );
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
   copyText.textContent = "copied";
   setTimeout(() => {
     copyText.innerHTML = `<span>&#128203; </span>copy`;
@@ -22,7 +23,6 @@ copyText.addEventListener("click", () => {
 const toggleSuccessModal = () => successModal.classList.toggle("active");
 const toggleErrorModal = () => errorModal.classList.toggle("active");
 window.addEventListener("click", function (event) {
-  if (event.target === successModal)
-    successModal.classList.remove("active");
+  if (event.target === successModal) successModal.classList.remove("active");
   if (event.target === errorModal) errorModal.classList.remove("active");
 });

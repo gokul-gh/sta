3bc9b422a49f13d282335da7139a550eff7c03d1
gokul <nkl.gokulb@gmail.com>
2024-06-10 12:01:55 +0530

    Chapter-9 splitting string

--- js/CH9-challenges/splitting_string/CH_ID_0032/CH_ID_0032/Splitting strings.js
+++ js/CH9-challenges/splitting_string/CH_ID_0032/CH_ID_0032/Splitting strings.js
@@ -1,3 +1,20 @@
+//Variables declaration
+let inputStringId = document.getElementById("stringId");
+let splitStringId = document.getElementById("searchResult");
+
+//Function to perform split method in a given string
+function stringFunction() {
+  let inputString = inputStringId.value;
+
+  splitStringId.value = inputString.split("");
+}
+
+//Function to clear input values
+function reset() {
+  inputStringId.value = "";
+  splitStringId.value = "";
+}
+
 //Modal
 const toggleSuccessModal = () => successModal.classList.toggle("active");
 const toggleErrorModal = () => errorModal.classList.toggle("active");
@@ -6,16 +23,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

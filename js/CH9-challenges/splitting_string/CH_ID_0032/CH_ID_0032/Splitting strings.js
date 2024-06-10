ed973b4d5e2462062e491a7ded9be2d3ab8f6fe6
gokul <nkl.gokulb@gmail.com>
2024-06-10 11:48:20 +0530

    Chapter-9 splitting string - Initial commit

---
+++ js/CH9-challenges/splitting_string/CH_ID_0032/CH_ID_0032/Splitting strings.js
@@ -0,0 +1,21 @@
+//Modal
+const toggleSuccessModal = () => successModal.classList.toggle("active");
+const toggleErrorModal = () => errorModal.classList.toggle("active");
+window.addEventListener("click", function (event) {
+  if (event.target === successModal) successModal.classList.remove("active");
+  if (event.target === errorModal) errorModal.classList.remove("active");
+});
+
+     //copy to clipboard
+const copyText = document.querySelector("#copy");
+      copyText.addEventListener("click", () => {
+        navigator.clipboard.writeText(
+          document.querySelector("#successCode").value
+        );
+        copyText.textContent = "copied";
+        setTimeout(() => {
+          copyText.innerHTML = `<span>&#128203; </span>copy`;
+        }, 2000);
+      });
+
+

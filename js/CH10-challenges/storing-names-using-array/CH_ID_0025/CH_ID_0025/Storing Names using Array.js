e32e2f876e1c53a0dbd7627c6c9ce5dd4d121b75
gokul <nkl.gokulb@gmail.com>
2024-06-11 22:49:10 +0530

    Chapter-10 Storing names using array

--- js/CH10-challenges/storing-names-using-array/CH_ID_0025/CH_ID_0025/Storing Names using Array.js
+++ js/CH10-challenges/storing-names-using-array/CH_ID_0025/CH_ID_0025/Storing Names using Array.js
@@ -1,4 +1,56 @@
 // Declaration
+
+//Input declaration for using variables method
+let actorNameId1 = document.getElementById("actorNameInput1");
+let actorNameId2 = document.getElementById("actorNameInput2");
+let actorNameId3 = document.getElementById("actorNameInput3");
+let outputusingVariables = document.getElementById("variableResultsId");
+
+//Input declaration for using array method
+let actorArrayNameId1 = document.getElementById("actorNameArrayInput1");
+let actorArrayNameId2 = document.getElementById("actorNameArrayInput2");
+let actorArrayNameId3 = document.getElementById("actorNameArrayInput3");
+let outputusingArrays = document.getElementById("arrayResultId");
+
+//Fuction to display names using variables method
+function printActorNames() {
+  outputusingVariables.innerHTML = "";
+  let actorName1 = actorNameId1.value;
+  let actorName2 = actorNameId2.value;
+  let actorName3 = actorNameId3.value;
+
+  outputusingVariables.innerHTML += `${actorName1}  ${actorName2} ${actorName3}`;
+}
+
+//Reset variables type function values
+function resetVariableInputs() {
+  actorNameId1.value = "";
+  actorNameId2.value = "";
+  actorNameId3.value = "";
+  outputusingVariables.innerHTML = "";
+}
+
+//Function to display names using array method
+function printArrayActorNames() {
+  outputusingArrays.innerHTML = "";
+  let actorArrayName = [
+    actorArrayNameId1.value,
+    actorArrayNameId2.value,
+    actorArrayNameId3.value,
+  ];
+  for (let iteration = 0; iteration < actorArrayName.length; iteration++) {
+    outputusingArrays.innerHTML += actorArrayName[iteration] + "  ";
+  }
+}
+
+//Function to reset values in array method
+function resetArrayInputs() {
+  actorArrayNameId1.value = "";
+  actorArrayNameId2.value = "";
+  actorArrayNameId3.value = "";
+  outputusingArrays.innerHTML = "";
+}
+
 // Screen date and time declaration.
 let displayDate = new Date();
 
@@ -19,5 +71,3 @@ copyText.addEventListener("click", () => {
     copyText.innerHTML = `<span>&#128203; </span>copy`;
   }, 2000);
 });
-
-//Input declaration

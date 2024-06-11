9c2fa1f23646f59521d93b32a24765a768108829
gokul <nkl.gokulb@gmail.com>
2024-06-11 21:07:05 +0530

    Chapter-10 Storing names using variables

---
+++ js/CH10-challenges/storing-names/CH_ID_0024/CH_ID_0024/Storing Names.js
@@ -0,0 +1,44 @@
+// Declaration
+let actorNameId1 = document.getElementById("actorNameInput1");
+let actorNameId2 = document.getElementById("actorNameInput2");
+let actorNameId3 = document.getElementById("actorNameInput3");
+let resultOutput = document.getElementById("variableResultsId");
+
+//Function to print input names in result
+function printNames() {
+  resultOutput.innerHTML = "";
+
+  let actorName = [actorNameId1.value, actorNameId2.value, actorNameId3.value];
+  for (iteration = 0; iteration < actorName.length; iteration++) {
+    resultOutput.innerHTML += actorName[iteration] + "  ";
+  }
+}
+
+//Function to reset all
+function reset() {
+  actorNameId1.value = "";
+  actorNameId2.value = "";
+  actorNameId3.value = "";
+  resultOutput.innerHTML = "";
+}
+
+// Screen date and time declaration.
+let displayDate = new Date();
+
+//Modal
+const toggleSuccessModal = () => successModal.classList.toggle("active");
+const toggleErrorModal = () => errorModal.classList.toggle("active");
+window.addEventListener("click", function (event) {
+  if (event.target === successModal) successModal.classList.remove("active");
+  if (event.target === errorModal) errorModal.classList.remove("active");
+});
+
+//copy to clipboard
+const copyText = document.querySelector("#copy");
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

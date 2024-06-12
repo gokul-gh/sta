e40285c85fd5492550f8949b635926f692dfbe78
gokul <nkl.gokulb@gmail.com>
2024-06-12 08:01:57 +0530

    Chapter-10 Storing array names using dynamic array

--- js/CH10-challenges/storing_names_array_dynamic/CH_ID_0026/CH_ID_0026/Storing Names in Array(Dynamic).js
+++ js/CH10-challenges/storing_names_array_dynamic/CH_ID_0026/CH_ID_0026/Storing Names in Array(Dynamic).js
@@ -1,4 +1,60 @@
 // Declaration
+
+//Input declaration
+//Declaration for inputs using variables
+let actorNameId1 = document.getElementById("actorNameInput1");
+let actorNameId2 = document.getElementById("actorNameInput2");
+let actorNameId3 = document.getElementById("actorNameInput3");
+let variableOutput = document.getElementById("variableResultsId");
+
+//Declaration for inputs using array
+let actorNameInputId = document.getElementById("actorsNameArrayInput");
+let actorNameOutput = document.getElementById("arrayResultId");
+let numberOfActorsAdded = document.getElementById("numberOfActorsAdded");
+
+//Function to print actor names using variables method
+function printActorNames() {
+  variableOutput.innerHTML = "";
+
+  let actorName1 = actorNameId1.value;
+  let actorName2 = actorNameId2.value;
+  let actorName3 = actorNameId3.value;
+
+  variableOutput.innerHTML = `${actorName1}\n ${actorName2}\n ${actorName3}\n`;
+}
+
+//Reset values in inputs using variables method
+function resetVariableInputs() {
+  variableOutput.innerHTML = "";
+  actorNameId1.value = "";
+  actorNameId2.value = "";
+  actorNameId3.value = "";
+}
+
+//Function to display names using array method
+let actorsArray = [];
+let iteration = 0;
+function addActorsName() {
+  actorsArray[iteration] = actorNameInputId.value;
+  iteration++;
+  numberOfActorsAdded.innerHTML = `No. of Actors added : ${iteration}`;
+}
+
+//Reset function to clear values using array method
+function printArrayActorNames() {
+  actorNameOutput.innerHTML = "";
+  for (let index = 0; index < iteration; index++) {
+    actorNameOutput.innerHTML += actorsArray[index] + "<br>";
+  }
+}
+
+function resetArrayInputs() {
+  actorNameInputId.value = "";
+  numberOfActorsAdded.innerHTML = "";
+  actorNameOutput.innerHTML = "";
+  iteration = 0;
+}
+
 // Screen date and time declaration.
 let displayDate = new Date();
 
@@ -19,5 +75,3 @@ copyText.addEventListener("click", () => {
     copyText.innerHTML = `<span>&#128203; </span>copy`;
   }, 2000);
 });
-
-//Input declaration

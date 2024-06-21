188670da275b9fc55fcab3e470819935e22d09b7
gokul <nkl.gokulb@gmail.com>
2024-06-21 09:35:21 +0530

    Chapter 3 - Variables - Initial commit

---
+++ js-advanced/fixIt/chapter-3/Variables/Variables /Variables.js
@@ -0,0 +1,15 @@
+  /*************************************************************************************
+ *  Name of the challenge  : Variables (Simple Counter App)                           *
+ *  Developed for          : SOFT TECH ASHRAM                                        *
+ *                                                                                   *
+ *  Developer                 Creation Date                        Activity          *
+ *                                                                                   *
+ *                                                                                   *
+ *                            Maintenance History                                    *
+ *                                                                                   *
+ *************************************************************************************/
+  //Date and Time declaration
+  let displayDate = new Date();
+  document.getElementById("dateOutput").innerHTML = displayDate.toLocaleDateString();
+  document.getElementById("timeOutput").innerHTML = displayDate.toLocaleTimeString();
+  //Code Statements

628b44e7f955ad85d3fc61606d49532fb121261a
gokul <nkl.gokulb@gmail.com>
2024-06-22 20:59:01 +0530

    Chapter 7 - DOM manipulation challenges

---
+++ js-advanced/challenges/chapter_7/CH_ID_0060/CH_ID_0060/Script files/Stone Paper Scissor.js
@@ -0,0 +1,13 @@
+/*            *************************************************************
+              *  Name of the challenge  : Stone Paper Scissor               *
+              *  Developed for          : VHITECH Training Program          *
+              *               Maintenance History                           *
+              *  Developer              :                                   *
+              *  Creation date           :                 Ticket No:       *
+              ************************************************************* */
+
+// Declaration
+// Screen date and time declaration.
+let displayDate = new Date();
+document.getElementById("dateOutput").innerHTML = displayDate.toLocaleDateString();
+document.getElementById("timeOutput").innerHTML = displayDate.toLocaleTimeString();

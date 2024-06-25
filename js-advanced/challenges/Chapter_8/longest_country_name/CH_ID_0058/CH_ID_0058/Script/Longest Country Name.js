f5b86c59eb0cc726b4cdacde4e8f890a175fddcd
gokul <nkl.gokulb@gmail.com>
2024-06-25 22:09:08 +0530

    Chapter 8 - Longest country name - Initial commit

---
+++ js-advanced/challenges/Chapter_8/longest_country_name/CH_ID_0058/CH_ID_0058/Script/Longest Country Name.js
@@ -0,0 +1,32 @@
+/*            *************************************************************
+              *   Name of the challenge: Longest Country Name             *
+              *            Developed by:                                  *
+              *           Programmed by:                                  *
+              *     Maintenance history:                  Ticket No:      *
+              *************************************************************  */
+
+// Declaration
+// Screen date and time declaration.
+let displayDate = new Date();
+//Modal
+const toggleSuccessModal = () => successModal.classList.toggle("active");
+const toggleErrorModal = () => errorModal.classList.toggle("active");
+window.addEventListener("click", function (event) {
+  if (event.target === successModal) successModal.classList.remove("active");
+  if (event.target === errorModal) errorModal.classList.remove("active");
+});
+
+     //copy to clipboard
+const copyText = document.querySelector("#copy");
+      copyText.addEventListener("click", () => {
+        navigator.clipboard.writeText(
+          document.querySelector("#successCode").value
+        );
+        copyText.textContent = "copied";
+        setTimeout(() => {
+          copyText.innerHTML = `<span>&#128203; </span>copy`;
+        }, 2000);
+      });
+
+
+

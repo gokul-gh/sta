51d151baa1275d5738ea570c07ca3db382c81af3
gokul <nkl.gokulb@gmail.com>
2024-06-26 09:15:11 +0530

    Chapter 9 - Factorial of a number

--- js-advanced/challenges/chapter_9/CH_ID_0043/CH_ID_0043/Factorial(Recursive).js
+++ js-advanced/challenges/chapter_9/CH_ID_0043/CH_ID_0043/Factorial(Recursive).js
@@ -1,12 +1,41 @@
 /*            *************************************************************
-              *  Name of the challenge   : Factorial : Recursive function  *
-              *  Developed for           : VHITECH Training Program        *
-              *               Maintenance History                          *
-              *  Developer               :                                 *
-              *  Creation date           :               Ticket No:        *
-              ************************************************************* */
+ *  Name of the challenge   : Factorial : Recursive function  *
+ *  Developed for           : VHITECH Training Program        *
+ *               Maintenance History                          *
+ *  Developer               :                                 *
+ *  Creation date           :               Ticket No:        *
+ ************************************************************* */
 
-// Declaration
+//Variables declaration
+let inputNumberId = document.getElementById("numberId");
+let result = document.getElementById("resultId");
+
+//Function to calculate factorial of a number
+function recursiveFunction() {
+  let factorial = 1,
+    iteration = 1;
+
+  let inputNumber = parseInt(inputNumberId.value);
+  recursive(inputNumber, factorial, iteration);
+}
+
+//Recursive function declaration to find factorial
+function recursive(inputNumber, factorial, iteration) {
+  if (iteration > inputNumber) {
+    result.value = factorial;
+    return;
+  }
+  factorial *= iteration;
+  recursive(inputNumber, factorial, iteration + 1);
+}
+
+//Reset function to clear all input values
+function reset() {
+  factorial = 1;
+  inputNumberId.value = "";
+  result.value = "";
+}
+
 // Date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -17,17 +46,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

50af2c822e7846509775d232f70b048665d924b0
gokul <nkl.gokulb@gmail.com>
2024-06-28 09:40:54 +0530

    Chapter-11 challenges - Object to array

--- js-advanced/challenges/chapter_11/objectToArray/CH_ID_0053 (1)/CH_ID_0053/Script/Object To Array.js
+++ js-advanced/challenges/chapter_11/objectToArray/CH_ID_0053 (1)/CH_ID_0053/Script/Object To Array.js
@@ -1,14 +1,58 @@
 /*            *************************************************************
-              *  Name of the challenge  : Object to Array                  *
-              *               Maintenance History                          *
-              *  Developer               :                                  *
-              *  Creation date           :               Ticket No:        *
-              ************************************************************ */
+ *  Name of the challenge  : Object to Array                  *
+ *               Maintenance History                          *
+ *  Developer               :                                  *
+ *  Creation date           :               Ticket No:        *
+ ************************************************************ */
 
+//Input declaration
+let objectKeyId = document.getElementById("keyId");
+let objectValueId = document.getElementById("valueId");
+let objectOutputArea = document.getElementById("displayObject");
+let arrayOutputArea = document.getElementById("displayArrayitems");
 
-// Declaration
+//Button declaration
+let addButton = document.getElementById("addButton");
+let convertButton = document.getElementById("pushButton");
+let resetuButton = document.getElementById("resetId");
+let objectVariable,
+  objectArray = [],
+  iteration = 0;
+
+//Function to add key values
+function addItems() {
+  let objectKey = parseInt(objectKeyId.value);
+  let objectValue = parseInt(objectValueId.value);
+
+  objectVariable = {
+    objectKey: objectKey,
+    objectValue: objectValue,
+  };
+  objectOutputArea.value += JSON.stringify(objectVariable);
+
+  objectArray[iteration] = objectVariable;
+  iteration++;
+}
+
+//Function to convert array into objects
+function convertObject() {
+  arrayOutputArea.value = "";
+  objectArray.forEach((position) => {
+    arrayOutputArea.value += `${Object.values(position)}\n`;
+  });
+}
+
+//Reset function to clear values
+function reset() {
+  objectKeyId.value = "";
+  objectValueId.value = "";
+  objectOutputArea.value = "";
+  arrayOutputArea.value = "";
+  objectArray = [];
+  iteration = 0;
+}
+
 // Screen date and time declaration.
-
 //Modal
 const toggleSuccessModal = () => successModal.classList.toggle("active");
 const toggleErrorModal = () => errorModal.classList.toggle("active");
@@ -17,18 +61,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
-//Input declaration
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

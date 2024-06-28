05f13257c754b53b5b1ee8a0f376d2a3691b30f1
gokul <nkl.gokulb@gmail.com>
2024-06-28 18:49:26 +0530

    Chapter-11 Sum of squares

--- js-advanced/challenges/chapter_11/sumOfSquares/CH_ID_0061/CH_ID_0061/Script/SumOfSquares.js
+++ js-advanced/challenges/chapter_11/sumOfSquares/CH_ID_0061/CH_ID_0061/Script/SumOfSquares.js
@@ -1,11 +1,51 @@
 /*            *************************************************************
-              *   Name of the challenge:                                     *
-              *            Developed by:                                     *
-              *           Programmed by:                                     *
-              *     Maintenance history:                     Ticket No:      *
-              *************************************************************  */
+ *   Name of the challenge:                                     *
+ *            Developed by:                                     *
+ *           Programmed by:                                     *
+ *     Maintenance history:                     Ticket No:      *
+ *************************************************************  */
 
-// Declaration
+//Variables Declaration
+let inputElementId = document.getElementById("arrayItems");
+let displayArrayId = document.getElementById("displayArray");
+let displaySquaresSum = document.getElementById("orderId");
+
+let inputElement,
+  arrayElement = [],
+  sum = 0;
+
+//Function to add input element to array
+let addItems = () => {
+  displayArrayId.value = "";
+  inputElement = parseInt(inputElementId.value);
+  arrayElement.push(inputElement);
+
+  displayArrayId.value += `[${arrayElement}]`;
+};
+
+//Function to find sum of squares
+let sumOfSquares = () => {
+  arrayElement.reduce(function (
+    accumulator,
+    currentValue,
+    currentIndex,
+    array,
+  ) {
+    sum += Math.pow(currentValue, 2);
+  }, 0);
+
+  displaySquaresSum.value = sum;
+};
+
+//Reset function to clear input values
+let reset = () => {
+  inputElementId.value = "";
+  displayArrayId.value = "";
+  displaySquaresSum.value = "";
+  arrayElement = [];
+  sum = 0;
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -16,17 +56,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

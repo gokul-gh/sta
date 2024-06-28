cd37d646e9275145547e1e2f5d8f30e73ba18bbf
gokul <nkl.gokulb@gmail.com>
2024-06-28 16:13:01 +0530

    Chapter-11 Challenges - Amount to coins

--- js-advanced/challenges/chapter_11/amountToCoins/CH_ID_0077/CH_ID_0077/Script/Amount To Coins.js
+++ js-advanced/challenges/chapter_11/amountToCoins/CH_ID_0077/CH_ID_0077/Script/Amount To Coins.js
@@ -1,11 +1,45 @@
 /*            *************************************************************
-              *   Name of the challenge:                                  *
-              *            Developed by:                                  *
-              *           Programmed by:                                  *
-              *     Maintenance history:                  Ticket No:      *
-              *************************************************************  */
+ *   Name of the challenge:                                  *
+ *            Developed by:                                  *
+ *           Programmed by:                                  *
+ *     Maintenance history:                  Ticket No:      *
+ *************************************************************  */
 
-// Declaration
+//Variables Declaration
+let inputAmountId = document.getElementById("amountId");
+let coinElementInputId = document.getElementById("arrayItems");
+let coinsArrayDisplay = document.getElementById("displayArray");
+let amountDisplay = document.getElementById("amountArray");
+
+let coinArray = [],
+  inputAmount;
+
+//Function to add input elements into an array
+let addItems = () => {
+  inputAmount = parseInt(inputAmountId.value);
+  let coinElementInput = parseInt(coinElementInputId.value);
+  coinArray.push(coinElementInput);
+  coinsArrayDisplay.value = coinArray;
+};
+
+//Function to calculate amount to coins
+let amountToCoins = () => {
+  amountDisplay.value = "";
+  coinArray.forEach((element) => {
+    amountDisplay.value += `${Math.floor(inputAmount / element)} `;
+    inputAmount %= element;
+  });
+};
+
+//Reset function to clear values
+let reset = () => {
+  inputAmountId.value = "";
+  coinElementInputId.value = "";
+  coinsArrayDisplay.value = "";
+  amountDisplay.value = "";
+  coinArray = [];
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -16,18 +50,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
-
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

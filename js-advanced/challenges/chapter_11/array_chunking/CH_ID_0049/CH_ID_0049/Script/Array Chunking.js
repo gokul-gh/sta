5668ddef2aa25e3a8cf3810d7cdfb34cde364c5c
gokul <nkl.gokulb@gmail.com>
2024-06-30 22:59:00 +0530

    Chapter-11 Challenges - Array chunking

--- js-advanced/challenges/chapter_11/array_chunking/CH_ID_0049/CH_ID_0049/Script/Array Chunking.js
+++ js-advanced/challenges/chapter_11/array_chunking/CH_ID_0049/CH_ID_0049/Script/Array Chunking.js
@@ -1,12 +1,55 @@
 /*            *************************************************************
-              *  Name of the challenge  : Array Chunking                   *
-              *               Maintenance History                          *
-              *  Developer              :                                  *
-              *  Creation date          :               Ticket No:         *
-              ************************************************************ */
+ *  Name of the challenge  : Array Chunking                   *
+ *               Maintenance History                          *
+ *  Developer              :                                  *
+ *  Creation date          :               Ticket No:         *
+ ************************************************************ */
 
+// Write a funtion to retun Array Chunking.
+// Example:
+//  chunking([1,2,3,4,5,],2);
+//  returns [[1,2],[3,4],[5]];"
 
 // Declaration
+let arrayInputId = document.getElementById("arrayItems");
+let displayArrayArea = document.getElementById("displayArray");
+let chunkSizeInputId = document.getElementById("pushItem");
+let chunkedArrayOutput = document.getElementById("orderId");
+let arrayValues = [],
+  chunkedArray = [];
+
+let addItems = () => {
+  let arrayInput = parseInt(arrayInputId.value);
+  arrayValues.push(arrayInput);
+  arrayInputId.value = "";
+  displayArrayArea.value = "";
+  displayArrayArea.value += `${arrayValues} `;
+};
+
+let calculate = () => {
+  chunkedArray = [];
+  let chunkSizeInput = parseInt(chunkSizeInputId.value);
+  for (
+    let iteration = 0;
+    iteration < arrayValues.length;
+    iteration += chunkSizeInput
+  ) {
+    chunkedArray.push(arrayValues.slice(iteration, iteration + chunkSizeInput));
+  }
+  chunkSizeInputId.value = "";
+  chunkedArrayOutput.value = "";
+  chunkedArrayOutput.value += JSON.stringify(chunkedArray);
+};
+
+let reset = () => {
+  arrayInputId.value = "";
+  displayArrayArea.value = "";
+  chunkSizeInputId.value = "";
+  chunkedArrayOutput.value = "";
+  arrayValues = [];
+  chunkedArray = [];
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -17,25 +60,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
-
-
-// Write a funtion to retun Array Chunking.
-// Example:
-//  chunking([1,2,3,4,5,],2);
-//  returns [[1,2],[3,4],[5]];"
-//Input declaration
-//Code Logic
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

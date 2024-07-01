5b545d93ba41e2b316d9f123be207b0c002d0c39
gokul <nkl.gokulb@gmail.com>
2024-07-01 08:08:49 +0530

    CH-11 push and shift method

--- js-advanced/challenges/chapter_11/push_and_shift_method/CH_ID_0038/CH_ID_0038/Script/Push and shift.js
+++ js-advanced/challenges/chapter_11/push_and_shift_method/CH_ID_0038/CH_ID_0038/Script/Push and shift.js
@@ -1,12 +1,55 @@
 /*            *************************************************************
-              *  Name of the challenge  : Push and Shift method            *
-              *               Maintenance History                          *
-              *  Developer              :                                  *
-              *  Creation date           :               Ticket No:        *
-              ************************************************************ */
+ *  Name of the challenge  : Push and Shift method            *
+ *               Maintenance History                          *
+ *  Developer              :                                  *
+ *  Creation date           :               Ticket No:        *
+ ************************************************************ */
 
+//Input Declaration
+let arrayInputId = document.getElementById("arrayItems");
+let arrayOutput = document.getElementById("displayArray");
+let pushInputId = document.getElementById("pushItem");
+let afterPushOutput = document.getElementById("displayArrayitems");
+let afterShiftOutput = document.getElementById("orderId");
+let arrayValues = [];
 
-// Declaration
+//Function to add inputs to array
+let addItems = () => {
+  let arrayInput = parseInt(arrayInputId.value);
+  if (arrayValues.length < 10) {
+    arrayValues.push(arrayInput);
+  }
+  arrayOutput.value = "";
+  arrayInputId.value = "";
+  arrayOutput.value = arrayValues;
+};
+
+//Function to push input to array
+let pushItems = () => {
+  let pushInput = parseInt(pushInputId.value);
+  arrayValues.push(pushInput);
+  pushInputId.value = "";
+  afterPushOutput.value = "";
+  afterPushOutput.value = arrayValues;
+};
+
+//Function to shift value from array
+let shift = () => {
+  arrayValues.shift();
+  afterShiftOutput.value = "";
+  afterShiftOutput.value = arrayValues;
+};
+
+//Reset function to clear values
+let reset = () => {
+  arrayInputId.value = "";
+  arrayOutput.value = "";
+  pushInputId.value = "";
+  afterPushOutput.value = "";
+  afterShiftOutput.value = "";
+  arrayValues = [];
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -17,17 +60,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

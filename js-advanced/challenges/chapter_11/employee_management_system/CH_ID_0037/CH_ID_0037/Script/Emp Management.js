16f24e5a55e65a699e7624431b2461b00a9b8bb9
gokul <nkl.gokulb@gmail.com>
2024-07-02 12:14:00 +0530

    CH11 - Employee Management System

--- js-advanced/challenges/chapter_11/employee_management_system/CH_ID_0037/CH_ID_0037/Script/Emp Management.js
+++ js-advanced/challenges/chapter_11/employee_management_system/CH_ID_0037/CH_ID_0037/Script/Emp Management.js
@@ -6,6 +6,93 @@
  *  Creation date           :     Ticket No:                  *
  **************************************************************/
 // Declaration
+
+//Input declaration
+let totalEmployeesId = document.getElementById("countId");
+let employeeNumberId = document.getElementById("empId");
+let employeeNameId = document.getElementById("nameId");
+let employeeDepartmentId = document.getElementById("deptId");
+
+let employeeOutput = document.getElementById("supplierTable");
+let sortedOutput = document.getElementById("sortTable");
+
+let searchInputId = document.getElementById("searchId");
+let searchOutput = document.getElementById("filterId");
+
+let getAllInputs = document.querySelectorAll("input");
+
+let employeeDetail = [],
+  count = 0;
+
+//Function to add employee detail to an array
+const addItems = () => {
+  let totalEmployees = parseInt(totalEmployeesId.value);
+  let employeeNumber = parseInt(employeeNumberId.value);
+  let employeeName = employeeNameId.value;
+  let employeeDepartment = employeeDepartmentId.value;
+
+  if (count < totalEmployees) {
+    employeeDetail.push([employeeNumber, employeeName, employeeDepartment]);
+
+    employeeDetail.forEach((row) => {
+      let trNew = document.createElement("tr");
+      row.forEach((column) => {
+        let tdNew = document.createElement("td");
+        tdNew.textContent = column;
+        trNew.appendChild(tdNew);
+      });
+      employeeOutput.appendChild(trNew);
+    });
+  }
+  count++;
+};
+
+//Function to sort array
+const sort = () => {
+  let afterSortArray = employeeDetail.sort();
+  afterSortArray.forEach((row) => {
+    let trNew = document.createElement("tr");
+    row.forEach((column) => {
+      let tdNew = document.createElement("td");
+      tdNew.textContent = column;
+      trNew.appendChild(tdNew);
+    });
+    sortedOutput.appendChild(trNew);
+  });
+};
+
+//Function to search for employee by name
+const employeeSearch = () => {
+  let searchInput = searchInputId.value;
+  searchOutput.value = "";
+  employeeDetail.forEach((element) => {
+    if (element[1] == searchInput) {
+      searchOutput.value += `${element}\n`;
+    }
+  });
+};
+
+//Function to clear values
+const reset = () => {
+  employeeDetail = [];
+  count = 0;
+  searchOutput.value = "";
+
+  while (employeeOutput.firstChild) {
+    employeeOutput.removeChild(employeeOutput.firstChild);
+  }
+  while (sortedOutput.firstChild) {
+    sortedOutput.removeChild(sortedOutput.firstChild);
+  }
+
+  getAllInputs.forEach((element) => {
+    //if condition is used because not to affect input field in success code
+    if (element.type) {
+      element.value = "";
+    }
+  });
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 
@@ -26,5 +113,3 @@ copyText.addEventListener("click", () => {
     copyText.innerHTML = `<span>&#128203; </span>copy`;
   }, 2000);
 });
-
-//Input declaration

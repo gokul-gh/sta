c62c89eb52e30853ec914f6dad544297de77de33
gokul <nkl.gokulb@gmail.com>
2024-07-03 23:06:57 +0530

    Sum of even numbers in a 2d matrix

--- js-advanced/challenges/chapter_11/sum_of_even_numbers_2d_matrix/CH_ID_0046/CH_ID_0046/Script files/Sum of even numbers.js
+++ js-advanced/challenges/chapter_11/sum_of_even_numbers_2d_matrix/CH_ID_0046/CH_ID_0046/Script files/Sum of even numbers.js
@@ -1,12 +1,80 @@
 /*
-*************************************************************
-*   Name of the challenge: Sum of the diagonals(2D Array)   *
-*            Challenge No: 23                               *
-*            Developed by: VHI tech                         *
-*           Programmed by: Kanagalakshmi                    *
-*     Maintenance history: Kanagalakshmi    Ticket No:      *
-*************************************************************  */
-// Declaration
+ *************************************************************
+ *   Name of the challenge: Sum of the diagonals(2D Array)   *
+ *            Challenge No: 23                               *
+ *            Developed by: VHI tech                         *
+ *           Programmed by: Kanagalakshmi                    *
+ *     Maintenance history: Kanagalakshmi    Ticket No:      *
+ *************************************************************  */
+//Variables Declaration
+let rowsColumnsInputId = document.getElementById("firstId");
+let evenSumOutput = document.getElementById("orderId");
+let gridId = document.getElementById("grid-container");
+gridId.style.display = "grid";
+
+let inputElement,
+  rowsColumnsInput,
+  evenSum = 0,
+  matrixValues = [],
+  finalMatrix = [];
+
+//Function to create input fields
+const getRow = (value) => {
+  rowsColumnsInput = parseInt(rowsColumnsInputId.value);
+
+  gridId.style.gridTemplateColumns = `repeat(${rowsColumnsInput}, 50px)`;
+  while (gridId.firstChild) {
+    gridId.removeChild(gridId.firstChild);
+  }
+
+  for (
+    let iteration = 1;
+    iteration <= rowsColumnsInput * rowsColumnsInput;
+    iteration++
+  ) {
+    inputElement = document.createElement("input");
+    inputElement.type = "number";
+
+    gridId.appendChild(inputElement);
+  }
+  evenSumOutput.value = "";
+};
+
+//Function to find sum of all even numbers
+const addItems = () => {
+  matrixValues = [];
+  finalMatrix = [];
+  evenSum = 0;
+  gridId.querySelectorAll("input").forEach((element) => {
+    matrixValues.push(parseInt(element.value));
+    if (element.value % 2 == 0) {
+      evenSum += parseInt(element.value);
+    }
+  });
+
+  for (
+    let iteration = 0;
+    iteration < matrixValues.length;
+    iteration += rowsColumnsInput
+  ) {
+    finalMatrix.push(
+      matrixValues.slice(iteration, iteration + rowsColumnsInput),
+    );
+  }
+
+  evenSumOutput.value = evenSum;
+};
+
+//Reset function to clear values and input tags
+const reset = () => {
+  rowsColumnsInputId.value = "";
+
+  while (gridId.firstChild) {
+    gridId.removeChild(gridId.firstChild);
+  }
+  evenSumOutput.value = "";
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 
@@ -18,19 +86,15 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});
 
 // "Create a function that returns the sum of all even elements in a 2D matrix.
 

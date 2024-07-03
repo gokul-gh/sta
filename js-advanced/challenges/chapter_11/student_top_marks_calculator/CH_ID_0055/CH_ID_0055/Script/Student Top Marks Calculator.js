f828b50a46b7535f2c9e024d220c60fc23cce1fe
gokul <nkl.gokulb@gmail.com>
2024-07-03 15:01:12 +0530

    Student top marks calculator

--- js-advanced/challenges/chapter_11/student_top_marks_calculator/CH_ID_0055/CH_ID_0055/Script/Student Top Marks Calculator.js
+++ js-advanced/challenges/chapter_11/student_top_marks_calculator/CH_ID_0055/CH_ID_0055/Script/Student Top Marks Calculator.js
@@ -1,12 +1,103 @@
 /*            *************************************************************
-              *  Name of the challenge  : Student Top Marks Calculator    *
-              *               Maintenance History                          *
-              *  Developer              :                                  *
-              *  Creation date           :               Ticket No:        *
-              ************************************************************ */
+ *  Name of the challenge  : Student Top Marks Calculator    *
+ *               Maintenance History                          *
+ *  Developer              :                                  *
+ *  Creation date           :               Ticket No:        *
+ ************************************************************ */
 
+//Variables Declaration
+let totalStudentsId = document.getElementById("sizeId");
+let studentKeyId = document.getElementById("keyId");
+let studentNameId = document.getElementById("nameId");
+let studentMarkId = document.getElementById("enterMarkId");
 
-// Declaration
+let studentMarksArrayOutput = document.getElementById("markArrayId");
+let objectsOutput = document.getElementById("displayObject");
+let topMarksOutput = document.getElementById("displayArrayitems");
+
+let getAllInputsId = document.querySelectorAll("input");
+let getAllTextArea = document.querySelectorAll("textarea");
+
+let totalStudents,
+  studentKey,
+  studentName,
+  studentMark,
+  studentMarksArray = [],
+  studentInfo = [],
+  studentObj;
+
+//Function to add marks to mark array
+const pushItems = () => {
+  totalStudents = parseInt(totalStudentsId.value);
+  studentMark = parseInt(studentMarkId.value);
+  if (studentMarksArray.length < 5) {
+    studentMarksArray.push(studentMark);
+  }
+  studentMarksArrayOutput.value = studentMarksArray;
+  studentMarkId.value = "";
+};
+
+//Function to add student object to an array
+const addItems = () => {
+  totalStudents = parseInt(totalStudentsId.value);
+  studentKey = studentKeyId.value;
+  studentName = studentNameId.value;
+
+  if (studentMarksArray.length == 0) {
+    studentMarksArray.push(0);
+  }
+
+  if (studentInfo.length < totalStudents) {
+    studentObj = {
+      id: studentKey,
+      name: studentName,
+      marks: studentMarksArray,
+    };
+    studentInfo.push(studentObj);
+  }
+  objectsOutput.value = JSON.stringify(studentInfo);
+
+  studentKeyId.value = "";
+  studentNameId.value = "";
+  studentMarkId.value = "";
+  studentMarksArrayOutput.value = "";
+  studentMarksArray = [];
+};
+
+//Function to output the student's largest marks in an array
+const convertObject = () => {
+  let topMarkArray = [];
+
+  studentInfo.forEach((individual) => {
+    individual.marks.sort(
+      (firstNumber, secondNumber) => firstNumber - secondNumber,
+    );
+    topMarkArray.push(individual.marks[individual.marks.length - 1]);
+  });
+
+  topMarksOutput.value = topMarkArray;
+};
+
+//Reset function to clear values
+const reset = () => {
+  (totalStudents = 0),
+    (studentKey = 0),
+    (studentName = ""),
+    (studentMark = 0),
+    (studentMarksArray = []),
+    (studentInfo = []),
+    studentObj;
+
+  getAllInputsId.forEach((element) => {
+    if (element.type) {
+      element.value = "";
+    }
+  });
+  getAllTextArea.forEach((element) => {
+    element.value = "";
+  });
+};
+
 // Screen date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -17,18 +108,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
-//Input declaration
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

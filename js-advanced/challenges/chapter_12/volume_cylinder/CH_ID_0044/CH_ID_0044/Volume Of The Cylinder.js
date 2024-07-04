70b944e094ecc27ece56d221206b7eb028f5393a
gokul <nkl.gokulb@gmail.com>
2024-07-04 21:37:18 +0530

    Volume of cylinder using class

--- js-advanced/challenges/chapter_12/volume_cylinder/CH_ID_0044/CH_ID_0044/Volume Of The Cylinder.js
+++ js-advanced/challenges/chapter_12/volume_cylinder/CH_ID_0044/CH_ID_0044/Volume Of The Cylinder.js
@@ -1,12 +1,47 @@
 /*            *************************************************************
-              *  Name of the challenge   : Volume of the cylinder (Class)   *
-              *               Maintenance History                           *
-              *  Developer               :                                  *
-              *  Creation date           :               Ticket No:         *
-              ************************************************************ */
+ *  Name of the challenge   : Volume of the cylinder (Class)   *
+ *               Maintenance History                           *
+ *  Developer               :                                  *
+ *  Creation date           :               Ticket No:         *
+ ************************************************************ */
 
+//Variables Declaration
+let radiusId = document.getElementById("firstNumber");
+let heightId = document.getElementById("secondNumber");
+let result = document.getElementById("resultId");
+let calculateButton = document.getElementById("buttonId");
+let getAllInputs = document.querySelectorAll("input");
 
-// Declaration
+//Function to calculate volume of a cylinder using class
+calculateButton.addEventListener("click", function (event) {
+  let radiusInput = parseInt(radiusId.value);
+  let heightInput = parseInt(heightId.value);
+
+  class Volume {
+    constructor(radius, height) {
+      this.radius = radius;
+      this.height = height;
+    }
+
+    displayOutput() {
+      const calculateVolume = Math.PI * Math.pow(this.radius, 2) * this.height;
+      result.value = calculateVolume;
+    }
+  }
+
+  const inputObj = new Volume(radiusInput, heightInput);
+  inputObj.displayOutput();
+});
+
+//Reset function to clear input fields
+document.getElementById("resetId").addEventListener("click", function (event) {
+  getAllInputs.forEach((element) => {
+    if (element.type) {
+      element.value = "";
+    }
+  });
+});
+
 // Screen date and time declaration.
 let displayDate = new Date();
 //Modal
@@ -17,19 +52,12 @@ window.addEventListener("click", function (event) {
   if (event.target === errorModal) errorModal.classList.remove("active");
 });
 
-     //copy to clipboard
+//copy to clipboard
 const copyText = document.querySelector("#copy");
-      copyText.addEventListener("click", () => {
-        navigator.clipboard.writeText(
-          document.querySelector("#successCode").value
-        );
-        copyText.textContent = "copied";
-        setTimeout(() => {
-          copyText.innerHTML = `<span>&#128203; </span>copy`;
-        }, 2000);
-      });
-
-
-
-
-//Input declaration
+copyText.addEventListener("click", () => {
+  navigator.clipboard.writeText(document.querySelector("#successCode").value);
+  copyText.textContent = "copied";
+  setTimeout(() => {
+    copyText.innerHTML = `<span>&#128203; </span>copy`;
+  }, 2000);
+});

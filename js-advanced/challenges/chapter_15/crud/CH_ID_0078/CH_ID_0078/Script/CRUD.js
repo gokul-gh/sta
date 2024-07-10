d6337229cc109b991f9c628edbad1d9b43a25146
gokul <nkl.gokulb@gmail.com>
2024-07-10 17:03:40 +0530

    CH15 CRUD - Initial commit

---
+++ js-advanced/challenges/chapter_15/crud/CH_ID_0078/CH_ID_0078/Script/CRUD.js
@@ -0,0 +1,30 @@
+/*            *************************************************************
+              *  Name of the challenge  : CRUD                             *
+              *  Developed for          :                                  *
+              *               Maintenance History                          *
+              *  Developer              :                                  *
+              *  Creation date           :               Ticket No:        *
+              ************************************************************* */
+//Modal
+const toggleSuccessModal = () => successModal.classList.toggle("active");
+const toggleErrorModal = () => errorModal.classList.toggle("active");
+window.addEventListener("click", function (event) {
+  if (event.target === successModal) successModal.classList.remove("active");
+  if (event.target === errorModal) errorModal.classList.remove("active");
+});
+
+     //copy to clipboard
+const copyText = document.querySelector("#copy");
+      copyText.addEventListener("click", () => {
+        navigator.clipboard.writeText(
+          document.querySelector("#successCode").value
+        );
+        copyText.textContent = "copied";
+        setTimeout(() => {
+          copyText.innerHTML = `<span>&#128203; </span>copy`;
+        }, 2000);
+      });
+
+
+
+//Code Statements

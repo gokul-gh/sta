9e99f8bad456c92c8c6b3c2686f922e4c242f1d3
gokul <nkl.gokulb@gmail.com>
2024-07-12 15:48:55 +0530

    CH15 - Data aggregation - Create headers using array method

--- js-advanced/challenges/chapter_15/data_aggregation/CH_ID_0078/CH_ID_0076/Script files/Data aggregation.js
+++ js-advanced/challenges/chapter_15/data_aggregation/CH_ID_0078/CH_ID_0076/Script files/Data aggregation.js
@@ -7,6 +7,83 @@
  ************************************************************* */
 
 // Declaration
+let supplierTableId = document.getElementById("supplierTable");
+let createTable = document.createElement("table");
+supplierTableId.appendChild(createTable);
+createTable.style.border = "2px black solid";
+createTable.style.borderCollapse = "collapse";
+
+let createHead = document.createElement("thead");
+let createTr = document.createElement("tr");
+let headerContents = [
+  "SupplierID",
+  "CompanyName",
+  "ContactName",
+  "Address",
+  "ProductName",
+  "UnitsInStock",
+  "UnitsOnOrder",
+  "UnitPrice",
+];
+let nameArray = [],
+  totalStockUnits = [],
+  totalOrderUnits = [],
+  totalPriceUnits = [];
+
+headerContents.forEach((element) => {
+  let createTh = document.createElement("th");
+  createTh.textContent = element;
+  createTr.appendChild(createTh);
+});
+
+createHead.appendChild(createTr);
+createTable.appendChild(createHead);
+
+let tableData = (element, createTr) => {
+  let createTd = document.createElement("td");
+  createTd.textContent = element;
+  createTr.appendChild(createTd);
+};
+
+supplierSales.forEach((element) => {
+  if (!nameArray[element.SupplierID - 1]) {
+    nameArray[element.SupplierID - 1] = [];
+    totalStockUnits[element.SupplierID - 1] = 0;
+    totalOrderUnits[element.SupplierID - 1] = 0;
+    totalPriceUnits[element.SupplierID - 1] = 0;
+  }
+  nameArray[element.SupplierID - 1].push(element.ProductName);
+  totalStockUnits[element.SupplierID - 1] += element.UnitsInStock;
+  totalOrderUnits[element.SupplierID - 1] += element.UnitsOnOrder;
+  totalPriceUnits[element.SupplierID - 1] += element.UnitPrice;
+});
+
+if (supplierJson.length > totalStockUnits.length) {
+  totalStockUnits.push(0);
+  totalOrderUnits.push(0);
+  totalPriceUnits.push(0);
+}
+
+let newArr = [];
+for (let iter = 0; iter < nameArray.length; iter++) {
+  newArr[iter] = nameArray[iter].join(",");
+}
+
+let createBody = document.createElement("tbody");
+supplierJson.forEach((element) => {
+  let createTr = document.createElement("tr");
+  tableData(element.SupplierID, createTr);
+  tableData(element.CompanyName, createTr);
+  tableData(element.ContactName, createTr);
+  tableData(element.Address, createTr);
+  tableData(newArr[element.SupplierID - 1], createTr);
+  tableData(totalStockUnits[element.SupplierID - 1], createTr);
+  tableData(totalOrderUnits[element.SupplierID - 1], createTr);
+  tableData(totalPriceUnits[element.SupplierID - 1], createTr);
+  createBody.appendChild(createTr);
+});
+
+createTable.appendChild(createBody);
 
 // Screen date and time declaration.
 let displayDate = new Date();

649c6498586b70d156a5e4b33567515e9c8e4d43
gokul <nkl.gokulb@gmail.com>
2024-07-18 11:11:29 +0530

    CH11 Bubble sort - fix: input checks

--- js-advanced/challenges/chapter_11/bubble_sort/CH_ID_0042/CH_ID_0042/Script files/Bubble sort.js
+++ js-advanced/challenges/chapter_11/bubble_sort/CH_ID_0042/CH_ID_0042/Script files/Bubble sort.js
@@ -16,58 +16,74 @@ let arrayDescendingOutput = document.getElementById("arraydescId");
 let arrayValues = [],
   bubbleArray = [];
 
+//Constant declaration
+const ERR_INPUT = "Enter valid input";
+const EMPTY_ARRAY = "Array is empty. Add values";
+const MAX_VALUE = 1000;
+
 //Function to add input to array
-let addItems = () => {
-  let arrayInput = parseInt(arrayInputId.value);
-  arrayValues.push(arrayInput);
+const addItems = () => {
+  let arrayInput = Number(arrayInputId.value);
+  if (arrayInputId.value != "" && arrayInput <= MAX_VALUE) {
+    arrayValues.push(arrayInput);
+  } else {
+    alert(ERR_INPUT);
+  }
   arrayInputId.value = "";
-  arrayOutputArea.value = "";
   arrayOutputArea.value = arrayValues;
   bubbleArray = arrayValues;
 };
 
 //Function to display array in ascending order and descending order using non-array method
-let bubbleSort = () => {
-  for (let outerIter = 0; outerIter < bubbleArray.length; outerIter++) {
-    for (let innerIter = 0; innerIter < bubbleArray.length; innerIter++) {
-      if (bubbleArray[outerIter] < bubbleArray[innerIter]) {
-        bubbleArray[outerIter] =
-          bubbleArray[outerIter] + bubbleArray[innerIter];
-        bubbleArray[innerIter] =
-          bubbleArray[outerIter] - bubbleArray[innerIter];
-        bubbleArray[outerIter] =
-          bubbleArray[outerIter] - bubbleArray[innerIter];
+const bubbleSort = () => {
+  if (bubbleArray.length != 0) {
+    for (let outerIter = 0; outerIter < bubbleArray.length; outerIter++) {
+      for (let innerIter = 0; innerIter < bubbleArray.length; innerIter++) {
+        if (bubbleArray[outerIter] < bubbleArray[innerIter]) {
+          bubbleArray[outerIter] =
+            bubbleArray[outerIter] + bubbleArray[innerIter];
+          bubbleArray[innerIter] =
+            bubbleArray[outerIter] - bubbleArray[innerIter];
+          bubbleArray[outerIter] =
+            bubbleArray[outerIter] - bubbleArray[innerIter];
+        }
       }
     }
-  }
-  nonArrayAscendingOutput.value = bubbleArray;
+    nonArrayAscendingOutput.value = bubbleArray;
 
-  for (let outerIter = 0; outerIter < bubbleArray.length; outerIter++) {
-    for (let innerIter = 0; innerIter < bubbleArray.length; innerIter++) {
-      if (bubbleArray[outerIter] > bubbleArray[innerIter]) {
-        bubbleArray[outerIter] =
-          bubbleArray[outerIter] + bubbleArray[innerIter];
-        bubbleArray[innerIter] =
-          bubbleArray[outerIter] - bubbleArray[innerIter];
-        bubbleArray[outerIter] =
-          bubbleArray[outerIter] - bubbleArray[innerIter];
+    for (let outerIter = 0; outerIter < bubbleArray.length; outerIter++) {
+      for (let innerIter = 0; innerIter < bubbleArray.length; innerIter++) {
+        if (bubbleArray[outerIter] > bubbleArray[innerIter]) {
+          bubbleArray[outerIter] =
+            bubbleArray[outerIter] + bubbleArray[innerIter];
+          bubbleArray[innerIter] =
+            bubbleArray[outerIter] - bubbleArray[innerIter];
+          bubbleArray[outerIter] =
+            bubbleArray[outerIter] - bubbleArray[innerIter];
+        }
       }
     }
+    nonArrayDescendingOutput.value = bubbleArray;
+  } else {
+    alert(EMPTY_ARRAY);
   }
-  nonArrayDescendingOutput.value = bubbleArray;
 };
 
 //Function to sort array in ascending and descending order based on array method
-let arraySort = () => {
-  arrayValues.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
-  arrayAscendingOutput.value = arrayValues;
+const arraySort = () => {
+  if (arrayValues.length != 0) {
+    arrayValues.sort((firstNumber, secondNumber) => firstNumber - secondNumber);
+    arrayAscendingOutput.value = arrayValues;
 
-  arrayValues.sort((firstNumber, secondNumber) => secondNumber - firstNumber);
-  arrayDescendingOutput.value = arrayValues;
+    arrayValues.sort((firstNumber, secondNumber) => secondNumber - firstNumber);
+    arrayDescendingOutput.value = arrayValues;
+  } else {
+    alert(EMPTY_ARRAY);
+  }
 };
 
 //Function to clear values
-let reset = () => {
+const reset = () => {
   arrayInputId.value = "";
   arrayOutputArea.value = "";
   nonArrayAscendingOutput.value = "";
